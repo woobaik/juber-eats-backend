3636fc80566047cdb80c631c549c329cc89c48ac
Joungwoo Baik <joungwoobaik@Joungwoos-Air.attlocal.net>
2021-12-01 10:03:21 -0500

    Add cross .env file for auto change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { RestaurantResolver } from './restaurants/restaurants.resolver';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'joungwoobaik',
+      password: '123',
+      database: 'Juber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
